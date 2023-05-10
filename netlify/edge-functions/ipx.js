59353682c8c9be0112022715223cd89d65860ff6
Zach Leatherman <zachleatherman@gmail.com>
2023-05-10 11:43:59 -0500

    test domain-level edge

--- netlify/edge-functions/ipx.js
+++ netlify/edge-functions/ipx.js
@@ -1,3 +1,4 @@
 export default () => new Response("Hello world Edge");
 
-export const config = { path: "/_next/image" };
+// export const config = { path: "/_next/image" };
+export const config = { path: "https://melodious-rugelach-38894a.netlify.app/_next/image" };
